15fc4a31e984d9f634cfc48bad0f41d417d940be
shriyanshjain23 <shriyanshjain2308@gmail.com>
2020-09-20 08:50:36 +0530

    c

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('dbcon.php');
-session_unset('user');
-session_destroy();
-header('location:index.php');
-?>
--- welcome.php
+++
@@ -1,4 +0,0 @@
-<?php
-include('login.php');
-echo $_SESSION['user'];
-?>
